9a55b8d47e69e6ddae943747028925d4a8dd8c13
msorce <msorce@users.noreply.github.com>
2016-04-14 14:23:50 -0700

    Update aboutCtrl.js

--- controllers/aboutCtrl.js
+++ controllers/aboutCtrl.js
@@ -2,5 +2,5 @@ angular
   .module('app')
   .controller('aboutCtrl', ['$scope', function($scope) {
     $scope.title = "Hobbies";
-    $scope.skills = ['Programming', 'Guitar', 'Cycilng', 'Fishing'];
+    $scope.skills = ['Programming', 'Guitar', 'Studying', 'Cycling', 'Fishing'];
   }]);
