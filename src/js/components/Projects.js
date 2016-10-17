fde67cc56b1f0397f28291cac3d1907516cd8594
msorce <marksorce@gmail.com>
2016-10-16 22:19:13 -0700

    added jsfiddles link

--- src/js/components/Projects.js
+++ src/js/components/Projects.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 export default class Projects extends React.Component {
+  constructor(props) {
+        super(props);
+        this.state = {active: false};
+    }
   render(){
     return (
       <div>
