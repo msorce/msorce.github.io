7c7dfd4e8c0e0f7013637f4b397e4d3ad0685e67
msorce <marksorce@gmail.com>
2016-10-17 19:44:12 -0700

    changed resume link and links order

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -10,11 +10,11 @@ export default class Header extends React.Component {
            <h1 class="title">Mark Sorce</h1>
            <h4><em>Front-End Software Engineer</em></h4>
            <ul class="list-inline text-center links">
+           <li><a href="https://www.linkedin.com/in/msorce" target="_blank" >linkedin</a></li>
              <li><a href="https://github.com/msorce" target="_blank" >github</a></li>
              <li><a href="https://jsfiddle.net/user/msorce/fiddles/" target="_blank">fiddles</a></li>
-             <li><a href="https://www.linkedin.com/in/msorce" target="_blank" >linkedin</a></li>
-             <li><a href="https://goo.gl/jnQSlE" target="_blank" >resume</a></li>
-             <li><a href="mailto:&#109;&#97;&#114;&#107;&#115;&#111;&#114;&#99;&#101;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;">contact</a></li>
+             <li><a href="https://goo.gl/3qfvPY" target="_blank" >resume</a></li>
+             <li><a href="mailto:&#109;&#97;&#114;&#107;&#115;&#111;&#114;&#99;&#101;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;">email</a></li>
            </ul >
          </div>
          <span class="arrow"> &darr; </span>
