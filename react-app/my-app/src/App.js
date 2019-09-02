9a74c62d08f055ecff703fffadfb5654ea1a17a2
msorce <msorce@wsgc.com>
2019-09-02 01:20:49 -0700

    updated background and links

---
+++ react-app/my-app/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import Particles from 'react-particles-js';
+import { particlesOptions } from "./particle-options";
+const date = new Date();
+
+function App() {
+  return (
+    <div className="App">
+      <Particles className="particles-container" 
+        params={particlesOptions}/>
+      <div className="name">MARK SORCE</div>
+      <nav>
+        <a href="https://goo.gl/3qfvPY" 
+          target="_blank" 
+          rel="noopener noreferrer">resume</a>
+        <a href="https://www.linkedin.com/in/msorce" 
+          target="_blank" 
+          rel="noopener noreferrer">linkedin</a>
+        <a href="https://github.com/msorce" 
+          target="_blank" 
+          rel="noopener noreferrer">github</a>
+        <a href="mailto:%6Dsorc%65ma%69%6Ch%61ndler@gm%61%69l.com" 
+          target="_blank" 
+          rel="noopener noreferrer">email</a>
+        <a href="https://codepen.io/msorce/" 
+          target="_blank" 
+          rel="noopener noreferrer">codepen</a>
+      </nav>
+      <copy> &copy; {date.getFullYear()} </copy>
+    </div>
+  );
+}
+
+export default App;
